741cd245ba26a11db6b51adae555baa392a2c7de
trialsolution <trial.solution@gmail.com>
2012-09-19 15:08:35 +0200

    focusing on Germany, different trend models

---
+++ src/germany.R
@@ -0,0 +1,45 @@
+library(plyr)
+
+ger <- subset(coco_uvap, country=="DE000000")
+
+ddply(ger,.(row),subset,value==min(value))
+
+
+ger_seed  <- subset(ger,row=="SEED")
+
+#linear trend model
+lm_seed  <- lm(value~year,data=ger_seed)
+summary(lm_seed)
+
+p <- ggplot(ger_seed,aes(year,value))
+p+geom_point()+stat_smooth(method="lm")
+
+#other simple trend models
+loglog_seed <- lm(log(value)~log(year),data=ger_seed)
+summary(loglog_seed)
+exp_seed <- lm(log(value)~year,data=ger_seed)
+summary(exp_seed)
+
+#linear trend with dummy
+ger_seed$dummy  <- (ger$year > 2005) * 1
+lm_seed  <- lm(value~year+dummy, data=ger_seed)
+summary(lm_seed)
+ger_seed  <- subset(ger,row=="SEED")qplot(ger_seed$year,lm_seed$residuals,geom="line")
+
+#test the normality of the residuals
+ks.test(lm_seed$residuals,"pnorm")
+
+
+#coefficient of variation ( sd / mean)
+
+#cov of the original time serie
+ger_seed$cov_orig  <- sd(ger_seed$value) / mean(ger_seed$value)
+#we should reduce it by identifying a time trend
+ger_seed$cov_detrended_dummy  <- sd(lm_seed$residuals) / mean(lm_seed$fitted.values)
+
+# !!! Important consequence: time-trend matters, i.e. the calculated c.o.v. depends highly on how well the trend fits
+
+lm_seed  <- lm(value~year,data=ger_seed)
+ger_seed$cov_detrended_linear  <- sd(lm_seed$residuals) / mean(lm_seed$fitted.values)
+ger_seed$cov_detrended_loglog  <- sd(loglog_seed$residuals) / mean(loglog_seed$fitted.values)
+ger_seed$cov_detrended_exp     <- sd(exp_seed$residuals) / mean(exp_seed$fitted.values)
